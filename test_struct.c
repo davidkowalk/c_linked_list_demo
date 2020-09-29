ae02b7bcc9dbea923280d55de2fe141e09a77370
PixelRayn <david.kowalk@gmail.com>
2020-09-29 20:54:00 +0200

    Added Demo Code

--- test_struct.c
+++ test_struct.c
@@ -33,3 +33,43 @@ void free_tree(struct node *node) {
   free(node);
   return;
 }
+
+//User Input
+void get_val(int *input) {
+  printf("node: ");
+  scanf("%d", input);
+}
+
+
+void main() {
+
+  //build head
+  struct node *head = malloc(sizeof(struct node));
+  struct node *last_element;
+  int input;
+
+  get_val(&input);
+  printf("head constructed.\n");
+
+  head -> n = input;
+  last_element = head;
+
+  //Add Children
+  while(input != -1) {
+    struct node *child = malloc(sizeof(struct node));
+    //printf("%p\n", child);
+
+    get_val(&input);
+
+    child -> n = input;
+    last_element -> child = child;
+    last_element = child;
+  }
+
+  last_element -> child = null;
+
+  print_node_tree(head);
+  printf("\n");
+  free_tree(head);
+
+}
